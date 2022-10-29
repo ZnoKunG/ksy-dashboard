60f6f6d617a53295899b1a8c40cf3f7459fbf3e4
ZnoKunG <zno.ksy@gmail.com>
2022-10-29 18:23:11 +0700

    create react app and fetch employees' birthday data and send emails

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import emailjs from '@emailjs/browser'
 import './App.css';
+const schedule = require('node-schedule')
 
-function App() {
+const sendEmployeeBirthDay = async () => {
+  const offset = 3
+  const resp = await fetch(`https://ksy-api.herokuapp.com/employee/birthday?offset=${offset}`)
+  const data = await resp.json()
+  const employeeNames = []
+  data.forEach(employee => {
+    employeeNames.push(employee["name"])
+  });
+  try {
+    if (employeeNames.length > 0) {
+      const sendResp = await emailjs.send("service_sdpje5r","ksy-birthday", {
+        to_email: 'zno.ksy@gmail.com',
+        offset: offset,
+        employee_list: employeeNames.toString()
+      }, "zrDvfvBlWz3V4tX7e")
+      console.log('SUCCESS!', sendResp.status, sendResp.text)
+    }
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const sendEmailJob = schedule.scheduleJob('25 * * * *', sendEmployeeBirthDay)
+
+const stopSendEmailJob = async () => {
+  sendEmailJob.cancel()
+}
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>KSY Dashboard</h1>
+    <button onClick={ stopSendEmailJob }>Stop Send Email Job</button>
+    </>
   );
 }
 
-export default App;
+
